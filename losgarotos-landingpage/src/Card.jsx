5202d8237f6ea09b5782b8edf69f735b3bd936bb
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2024-12-02 02:13:12 +0000

    NuevoCommit

--- losgarotos-landingpage/src/Card.jsx
+++ losgarotos-landingpage/src/Card.jsx
@@ -8,7 +8,7 @@ export const Card = () => {
   return (
     <div className="container-fluid p-2">
       <div className="row justify-content-center">
-{/* card #1 */}
+        {/* card #1 */}
         <div className="col-12 col-md-3 mb-3">
           <div className="card border mb-3">
             <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0016/0004/brand.gif?itok=p6ewe6-E" className="card-img-top" alt="..." />
@@ -16,9 +16,12 @@ export const Card = () => {
               <h5 className="card-title">Título</h5>
               <p className="card-text text-center">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
             </div>
+            <div className="card-footer bg-dark-subtle d-flex justify-content-center">
+              <a href="#" className="button btn btn-primary">Go somewhere</a>
+            </div>
           </div>
         </div>
-{/* card #2 */}
+        {/* card #2 */}
         <div className="col-12 col-md-3 mb-3">
           <div className="card border mb-3">
             <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0016/0004/brand.gif?itok=p6ewe6-E" className="card-img-top" alt="..." />
@@ -26,9 +29,12 @@ export const Card = () => {
               <h5 className="card-title">Título</h5>
               <p className="card-text text-center">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
             </div>
+            <div className="card-footer bg-dark-subtle d-flex justify-content-center">
+              <a href="#" className="button btn btn-primary">Go somewhere</a>
+            </div>
           </div>
         </div>
-{/* card #3 */}
+        {/* card #3 */}
         <div className="col-12 col-md-3 mb-3">
           <div className="card border mb-3">
             <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0016/0004/brand.gif?itok=p6ewe6-E" className="card-img-top" alt="..." />
@@ -36,21 +42,27 @@ export const Card = () => {
               <h5 className="card-title">Título</h5>
               <p className="card-text text-center">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
             </div>
+            <div className="card-footer bg-dark-subtle d-flex justify-content-center">
+              <a href="#" className="button btn btn-primary">Go somewhere</a>
+            </div>
           </div>
         </div>
-{/* card #4 */}
+        {/* card #4 */}
         <div className="col-12 col-md-3 mb-3">
           <div className="card border mb-3">
             <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0016/0004/brand.gif?itok=p6ewe6-E" className="card-img-top" alt="..." />
             <div className="card-body border d-flex flex-column align-items-center">
               <h5 className="card-title">Título</h5>
               <p className="card-text text-center">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+            </div>
+            <div className="card-footer bg-dark-subtle d-flex justify-content-center">
+              <a href="#" className="button btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
       </div>
     </div>
-        );
+  );
 };
 
 export default Card;
