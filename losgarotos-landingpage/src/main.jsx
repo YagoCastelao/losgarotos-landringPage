652b18eafc49510226fe46eb740b5ed1db3d60d4
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-04 11:38:14 +0100

    refactorando el proyecto

--- losgarotos-landingpage/src/main.jsx
+++ losgarotos-landingpage/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Navbar from './Navbar.jsx'
-import Jumbotron from './Jumbotron-button.jsx'
-import Modal from './Modal.jsx'
-import Card from './Card.jsx'
-import Footer from './Footer.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Jumbotron from './components/Jumbotron/Jumbotron-button.jsx'
+import Modal from './components/Modal/Modal.jsx'
+import Card from './components/Card/Card.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
